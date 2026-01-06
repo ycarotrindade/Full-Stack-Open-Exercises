fae19f52173a2c850e28b4cd184ad4834bad0427
ycarotrindade <ycarobanes@gmail.com>
2026-01-05 21:47:42 -0400

    Adding a course object

--- Part_1/courseinfo/src/App.jsx
+++ Part_1/courseinfo/src/App.jsx
@@ -34,26 +34,29 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = "Half Stack application developement"
-  const part1 = {
-    name:"Fundamentals of React",
-    exercises: 10
+  const course = {
+    name:"Half Stack application developement",
+    parts: [
+      {
+        name:"Fundamentals of React",
+        exercises: 10
+      },
+      {
+        name:"Using props to pass data",
+        exercises:7
+      },
+      {
+        name:"State of a component",
+        exercises:14
+      }
+    ]
   } 
-  const part2 = {
-    name:"Using props to pass data",
-    exercises:7
-  }
-  const part3 = {
-    name:"State of a component",
-    exercises:14
-  }
-  let parts = [part1, part2, part3]
   let sumOfExercises = 0
-  parts.forEach(value =>{sumOfExercises += value.exercises})
+  course.parts.forEach(value =>{sumOfExercises += value.exercises})
   return (
     <div>
-      <Header course={course}/>
-      <Content parts={parts}/>
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
       <Total sumExercises={sumOfExercises}/>
     </div>
   )
