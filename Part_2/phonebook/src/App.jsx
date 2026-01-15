4d0ab67ae5f18572f61d242d2a5dcdd64969b8ac
ycarotrindade <ycarobanes@gmail.com>
2026-01-15 18:06:05 -0400

    First commit to phonebook exercise

---
+++ Part_2/phonebook/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+
+  const AddPerson = (event) => {
+    event.preventDefault()
+    const newPerson = {name:newName}
+    setPersons(persons.concat(newPerson))
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={AddPerson}>
+        <div>
+          name: <input value={newName} onChange={(event) => {setNewName(event.target.value)}} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person, i) => <p key={i}>{person.name}</p>)}
+    </div>
+  )
+}
+
+export default App
