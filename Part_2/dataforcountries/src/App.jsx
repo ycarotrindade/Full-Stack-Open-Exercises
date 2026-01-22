4abb4ee11468836179fa24c9e5a33ad0e5ee3c70
ycarotrindade <ycarobanes@gmail.com>
2026-01-21 21:28:59 -0400

    Adding Country widget and changing the logic

--- Part_2/dataforcountries/src/App.jsx
+++ Part_2/dataforcountries/src/App.jsx
@@ -15,10 +15,11 @@ const App = () => {
     setFilter(event.target.value)
   }
 
+  const countriesToShow = countries === null ? countries : countries.filter(country => country.name.common.toLowerCase().includes(filter.toLowerCase()))
   return (
     <div>
       <label htmlFor="filter">find countries </label><input id="filter" type="text" value={filter} onChange={onChangeFilter}/>
-      <CountryList countriesToShow={countries} filter={filter}/>
+      <CountryList countriesToShow={countriesToShow}/>
     </div>
   )
 }
--- Part_2/dataforcountries/src/components/CountryList.jsx
+++ Part_2/dataforcountries/src/components/CountryList.jsx
@@ -1,15 +1,33 @@
-const CountryList = ({countriesToShow, filter}) => {
+const Country = ({country}) => {
+    return (
+        <div>
+            <h1>{country.name.common}</h1>
+            <p>Capital {country.capital[0]}</p>
+            <p>Area {country.area}</p>
+            <h2>Languages</h2>
+            <ul>
+                {Object.entries(country.languages).map(([key,value]) => {return <li key={key}>{value}</li>})}
+            </ul>
+            <img src={country.flags.svg} alt={country.flags.alt} width="300px" />
+        </div>
+    )
+}
 
-    if(countriesToShow === null) {
+const CountryList = ({countriesToShow}) => {
+
+    if(countriesToShow === null || countriesToShow.length === 0 ){
         return (
             <div>
                 <p>No countries to show</p>
             </div>
         )
-    }
-
-    const countriesToShowFiltered = countriesToShow.filter(country => country.name.common.toLowerCase().includes(filter.toLowerCase()))
-    if(countriesToShowFiltered.length > 10){
+    }else if (countriesToShow.length === 1) {
+        return (
+            <>
+            <Country country={countriesToShow[0]}/>
+            </>
+        )
+    }else if(countriesToShow.length > 10){
         return (
             <div>
                 <p>Too many matches, specify another filter</p>
@@ -18,7 +36,7 @@ const CountryList = ({countriesToShow, filter}) => {
     }else{
         return (
             <div>
-                {countriesToShowFiltered.map(country => <p key={country.flag}>{country.name.common}</p>)}
+                {countriesToShow.map(country => <p key={country.flag}>{country.name.common}</p>)}
             </div>
         )
     }
