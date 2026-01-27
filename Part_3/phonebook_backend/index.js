5576469c2320f074545acd79cd6cd61e930633ec
ycarotrindade <ycarobanes@gmail.com>
2026-01-27 19:41:05 -0400

    Using custom log token

--- Part_3/phonebook_backend/index.js
+++ Part_3/phonebook_backend/index.js
@@ -2,8 +2,14 @@ const express = require("express")
 const morgan = require('morgan')
 const app = express()
 
+morgan.token('body', request => {
+  if(request.method === 'POST'){
+    return JSON.stringify(request.body)
+  }
+})
+
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     { 
