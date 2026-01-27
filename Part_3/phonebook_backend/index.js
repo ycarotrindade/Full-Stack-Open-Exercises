f9b17c8351c23fdcea29d62e30c8e43bde50f3c9
ycarotrindade <ycarobanes@gmail.com>
2026-01-27 18:51:20 -0400

    Adding add person functionality

--- Part_3/phonebook_backend/index.js
+++ Part_3/phonebook_backend/index.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
       "id": "1",
@@ -54,7 +56,21 @@ app.delete('/api/persons/:id', (request, response) => {
     persons = persons.filter(person => person.id !== id)
     response.json(person)
   }
+})
 
+app.post('/api/persons', (request, response) => {
+  const {name, number} = request.body
+  if(!name || !number){
+    response.status(400).end()
+    return
+  }
+  const newPerson = {
+    id:`${Math.floor(Math.random() * 1000000)}`, 
+    name:name, 
+    number:number
+  }
+  persons = persons.concat(newPerson)
+  response.status(201).json(newPerson)
 })
 
 const PORT = 3001
